ea46f1d032f71d2a61a34714605961e6e6a71205
David Umoru <umorudavido@gmail.com>
2023-08-25 02:31:54 +0100

    Remove ui provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 "use client"
 import React from 'react';
-import { NextUIProvider } from "@nextui-org/react";
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      <NextUIProvider>
-        <h1>Mood Fusion</h1>
-      </NextUIProvider>
+      <h1>Mood Fusion</h1>
     </div>
   )
 }
